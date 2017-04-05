3a70fb40decd38ca7193ddce5113fe717c1c1a7f
Ran Navok <rnavok@gmail.com>
2017-04-05 15:54:10 +0300

    port to PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('currentYear',()=>{
